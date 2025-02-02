c7b86ce353ddd2e75236efb88878215fc4236a03
Tanya Vardhan <tanya@Tanyas-MacBook-Air.local>
2025-02-02 17:44:27 +0530

    Added gallery Section

---
+++ rota-website/src/data/eventsData.js
@@ -0,0 +1,53 @@
+const eventsData=[
+    {
+        name:"Vriddhi",
+        date:"04-01-2022",
+        images:[
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+        ]
+    },
+    {
+        name:"Rota Olympics",
+        date:"02-10-2021",
+        images:[
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+            "/assets/RotaOlympics/random_pic2.JPG",
+        ]
+    },
+
+    {
+        name:"Sapna",
+        date:"03-08-2020",
+        images:[
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+            "/assets/Vriddhi/random_pic.jpg",
+        ] 
+    },
+];
+
+export default eventsData;
