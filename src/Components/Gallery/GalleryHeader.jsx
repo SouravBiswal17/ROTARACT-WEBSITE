66c8f70db18f11ce49df520a870bf74f3d7a7848
BlessedAmrita <blessedamrita9@gmail.com>
2025-03-04 21:28:23 +0530

    feat : add gallery section

---
+++ src/Components/Gallery/GalleryHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GalleryHeader() {
+  return (
+    <>
+    <div className="py-[18px] md:py-[25px] bg-[#DAEAF1] flex justify-center items-center text-[36px] md:text-[45px] font-semibold text-[#006E99] ">GALLERY</div>
+    </>
+  )
+}
+
+export default GalleryHeader
